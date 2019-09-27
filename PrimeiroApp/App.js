5aea0dd4b572775a0c7ec1d777be71b1c5e9884c
Alex Pinheiro <alexpinheiropf@hotmail.com>
2019-09-26 22:52:45 -0300

    Primeiros ajustes

--- PrimeiroApp/App.js
+++ PrimeiroApp/App.js
@@ -1,11 +1,22 @@
 import React, {Component} from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 export default class App extends Component{
   render() {
+    let nome = 'Alex';
+    let img = {
+      uri: 'https://sujeitoprogramador.com/steve.png'
+    };
     return (
       <View>
-        <Text>Hello World</Text>
+        <Text>Olá!</Text>
+        <Text>Tudo bem!</Text>
+        <Text>Tudo e Você?</Text>
+        <Text style={{fontSize:25, color: 'red', margin: 20}}>Tudo e Ótimo!</Text>
+
+        <Image source={img} 
+                style={{width:200, height: 200}} />
+        <Text>{nome}</Text>
       </View>
     );
   }
