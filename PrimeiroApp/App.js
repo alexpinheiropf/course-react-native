3b3f996bf5d34e6e410e6225cad03e1e49767f5d
Alex Pinheiro <alexpinheiropf@hotmail.com>
2019-09-27 07:13:09 -0300

    Componentes reutilizavéis passando props

--- PrimeiroApp/App.js
+++ PrimeiroApp/App.js
@@ -1,21 +1,32 @@
 import React, {Component} from 'react';
 import { View, Text, Image } from 'react-native';
 
-export default class App extends Component{
+class Jobs extends Component {
   render() {
-    let nome = 'Alex';
     let img = {
       uri: 'https://sujeitoprogramador.com/steve.png'
     };
+    return(
+      <Image source={img} style={{
+        width: parseInt(this.props.largura), 
+        height: parseInt(this.props.altura)
+      }} />
+    );
+  }
+}
+
+export default class App extends Component{
+  render() {
+    let nome = 'Alex';
     return (
       <View>
         <Text>Olá!</Text>
         <Text>Tudo bem!</Text>
         <Text>Tudo e Você?</Text>
         <Text style={{fontSize:25, color: 'red', margin: 20}}>Tudo e Ótimo!</Text>
-
-        <Image source={img} 
-                style={{width:200, height: 200}} />
+        
+        <Jobs largura='500' altura='150'/>
+        
         <Text>{nome}</Text>
       </View>
     );
