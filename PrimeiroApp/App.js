91095271c388668150f9b894143373d48787c99f
Alex Pinheiro <alexpinheiropf@hotmail.com>
2019-09-27 08:00:23 -0300

    FlexDirection

--- PrimeiroApp/App.js
+++ PrimeiroApp/App.js
@@ -4,11 +4,11 @@ export default class App extends Component{
 
   render() {
     return (
-      <View style={{flex: 1}}>
+      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
 
-      <View style={{flex:1, backgroundColor: 'green'}}></View>
-      <View style={{flex:1, backgroundColor: 'yellow'}}></View>
-      <View style={{flex:2, backgroundColor: 'blue'}}></View>
+      <View style={{width: 50, height: 50, backgroundColor: 'red'}}></View>
+      <View style={{width: 50, height: 50, backgroundColor: 'green'}}></View>
+      <View style={{width: 50, height: 50, backgroundColor: 'blue'}}></View>
 
       </View>
     );
