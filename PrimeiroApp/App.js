1a851c48bb7192da55d4a703fbf4cd3b35ca737d
Alex Pinheiro <alexpinheiropf@hotmail.com>
2019-09-27 07:51:38 -0300

    Utilizando Styles flexiveis

--- PrimeiroApp/App.js
+++ PrimeiroApp/App.js
@@ -1,28 +1,16 @@
 import React, {Component} from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
 export default class App extends Component{
 
   render() {
     return (
-      <View>
-        <Text style={styles.textoPrincipal}> Eu sou o texto 1</Text>
-        <Text style={styles.textoColor}> Eu sou o texto 2</Text>
-        <Text style={[styles.textoColor, styles.subTexto]}> Eu sou o texto 3</Text>
-        <Text style={[styles.textoPrincipal, styles.textoColor]}> Eu sou o texto 4</Text>
+      <View style={{flex: 1}}>
+
+      <View style={{flex:1, backgroundColor: 'green'}}></View>
+      <View style={{flex:1, backgroundColor: 'yellow'}}></View>
+      <View style={{flex:2, backgroundColor: 'blue'}}></View>
+
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-    textoPrincipal:{
-      fontSize: 27,
-      textAlign: 'center'
-    },
-    textoColor:{
-      color:'red'
-    },
-    subTexto:{
-      textAlign: 'right'
-    }
-});
