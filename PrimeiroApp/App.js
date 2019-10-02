69e24f1d7a5b0c008af9f1e38f79cba42f4bce6e
Alex Pinheiro <alex.pinheiro@compasso.com.br>
2019-10-02 07:57:42 -0300

    Desenvolvendo um biscoito da sorte com frases randomicas no array

--- PrimeiroApp/App.js
+++ PrimeiroApp/App.js
@@ -1,37 +1,83 @@
 import React, {Component} from 'react';
-import { View, TextInput, StyleSheet, Text, Button } from 'react-native';
-export default class App extends Component{
+import { View, StyleSheet, Text, TouchableOpacity, Image } from 'react-native';
 
+class Botao extends Component{
   constructor(props){
     super(props);
-    this.state = {
-      texto: '',
-      textoInput: ''
-    };
-    this.enviar = this.enviar.bind(this);
+    this.state ={};
 
+    this.styles = StyleSheet.create({
+      botao: {
+        width: 230,
+        height: 50,
+        borderWidth: 2,
+        borderColor: props.cor,
+        borderRadius: 25
+      },
+      btnArea: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+      },
+      btnTexto: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: props.cor
+      }
+    });
   }
+  render(){
+    return(
+      <TouchableOpacity style={this.styles.botao} onPress={this.props.eventoBotao}>
+        <View style={this.styles.btnArea}>
+          <Text style={this.styles.btnTexto}>{this.props.nome}</Text>
+        </View>
+      </TouchableOpacity>
+    )
+  }
+}
 
-  enviar(){
+export default class App extends Component{
+constructor (props){
+  super(props);
+  this.state = {
+    textoFrase: ''
+  };
+
+  this.quebrarBiscoito = this.quebrarBiscoito.bind(this);
+
+  this.frases = ['Siga os bons e aprenda com eles.', 'O bom-senso vale mais do que muito conhecimento.', 
+  'O riso é a menor distância entre duas pessoas.', 
+  'Deixe de lado as preocupações e seja feliz.',
+  'Realize o óbvio, pense no improvável e conquiste o impossível.',
+  'Acredite em milagres, mas não dependa deles.',
+  'A maior barreira para o sucesso é o medo do fracasso.'];
+}
+
+  quebrarBiscoito(){
     let state = this.state;
-    state.texto = 'Bem-vindo ' + state.textoInput;
-    alert(state.texto)
 
-    this.setState(state);
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+
+    // Recebendo o numero da frase proporcional a posição no array
+    state.textoFrase =' " ' + this.frases[numeroAleatorio] + ' "';
+
+    this.setState(state)
   }
-
 
   render() {
     return (
       <View style={styles.container}>
-        <TextInput style={styles.input} 
-                    placeholder="Digite seu nome..." 
-                    underlineColorAndroid="transparent" 
-                    onChangeText={(textoInput=> this.setState({textoInput}))}/>
 
-        <Button title="Aperte" onPress={this.enviar}/>  
+        <Image source={require('./src/biscoito.png')} style={styles.img}/>
+
+        <Text style={styles.textoFrase}>{this.state.textoFrase}</Text>
         
-        <Text style={styles.text}>{this.state.texto}</Text>   
+       <Botao cor="#dd7b22" nome="Abrir Biscoito" eventoBotao={this.quebrarBiscoito}/>
+
+       {/* <Botao cor="#000000" nome="Sair do App"/> */}
+          
       </View>
     );
   }
@@ -39,20 +85,20 @@ export default class App extends Component{
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    paddingTop: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  input: {
-    height: 45,
-    borderWidth: 1,
-    borderColor: '#000000',
-    margin: 20,
-    padding: 10
+  img: {
+    width: 250,
+    height: 250
   },
-  text: {
+  textoFrase: {
+    fontSize: 20,
+    color: '#dd7b22',
+    margin: 30,
     textAlign: 'center',
-    fontSize: 23,
-    color: '#000000',
-    marginTop: 15
-
+    fontStyle: 'italic'
   }
 });
